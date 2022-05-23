54c6e022ef28b16eae4dab7f19d6a4008ec06a7b
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-23 16:17:57 +0600

    connect with mongodb and declare run function

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuqyw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello From Hardware Tools!');
